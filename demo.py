4ce578dca0456d9135a076d9b85131793b435ed4
root <root@localhost.localdomain>
2024-05-18 12:49:47 -0400

    Developer 1 edited this

--- demo.py
+++ demo.py
@@ -1,2 +1,3 @@
-print("Hello this is a git file")
-print("Now we have edited this git file")
+print("OH! Hell, this is a git file")
+print("Now we have again edited this git file")
+print("From developer 1 branch")
